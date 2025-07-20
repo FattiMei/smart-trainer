4671fa83acb2197773581451503995a6f09c7f0a
FattiMei <teomei68@gmail.com>
2025-07-20 08:01:32 +0200

    refactor: move loader functions in separate script

---
+++ analysis/loader.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+
+
+dataset_folder = '../datasets'
+sensors = ['Infineon', 'SR250Mate']
+experiments = ['apnea', 'breathing', 'foreign', 'intensity', 'misc', 'recovery', 'rpm-ladder', 'signal-to-noise']
+
+
+def get_experiment_names(sensor: str, experiment: str) -> tuple[str, list[str]]:
+    base_folder_path = os.path.join(dataset_folder, sensor, experiment)
+    experiment_list = os.listdir(base_folder_path)
+
+    return base_folder_path, experiment_list
+
+
+def load_experiment(base_folder_path: str, name: str) -> np.ndarray:
+    complete_path = os.path.join(base_folder_path, name)
+
+    return np.load(complete_path)
