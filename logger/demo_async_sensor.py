9cec92839a66379ae366cbed71968f742bb9a3f6
FattiMei <teomei68@gmail.com>
2025-09-01 22:08:23 +0200

    Add working async sensor and visualization, with collection task cancellable from timeout and from main thread

--- logger/demo_async_sensor.py
+++ logger/demo_async_sensor.py
@@ -15,30 +15,34 @@ class SimulatedLineSensor(AbstractSensor):
         super().__init__()
         self.window = SlidingWindow(window_size_seconds)
         self.init_visualization(ax)
+        self.start_time = None
 
-    async def stop_collection(self):
-        pass
-
-    async def _run(self):
+    async def collect(self, start_time: float, duration_seconds: float):
+        self.start_time = start_time
         state = CollectionState.START
 
-        while self.state != CollectionState.STOP:
-            if self.state == CollectionState.WAIT:
-                assert(False)
+        try:
+            async with asyncio.timeout(duration_seconds):
+                while True:
+                    if state == CollectionState.START:
+                        state = CollectionState.READ
 
-            elif self.state == CollectionState.START:
-                self.state = CollectionState.READ
+                    elif state == CollectionState.READ:
+                        await asyncio.sleep(np.random.uniform())
+                        timestamp = time.perf_counter() - self.start_time
+                        data = np.sin(timestamp) + 0.1*np.random.uniform()
 
-            elif self.state == CollectionState.READ:
-                await asyncio.sleep(np.random.uniform())
-                timestamp = time.perf_counter() - self.start_time
-                data = np.sin(timestamp) + 0.1*np.random.uniform()
+                        self.window.push(timestamp, data)
+                        self.update_visualization_data((
+                            self.window.timeq,
+                            self.window.dataq
+                        ))
 
-                self.window.push(timestamp, data)
-                self.update_visualization_data((
-                    self.window.timeq,
-                    self.window.dataq
-                ))
+                    elif state == CollectionState.STOP:
+                        break
+
+        except (TimeoutError, asyncio.CancelledError):
+            print('sensor collection terminated')
 
     def init_visualization(self, ax):
         ax.set_title('Async line sensor')
@@ -52,12 +56,13 @@ class SimulatedLineSensor(AbstractSensor):
     # senza passare per animation.FuncAnimation
     # in questo modo potrei integrare meglio la libreria Vispy
     def update_visualization(self, t: float):
-        deltat = np.clip(
-            t - self.start_time - self.window.seconds,
-            0.0,
-            np.inf
-        )
-        self.ax.set_xlim((deltat, deltat + self.window.seconds))
+        if self.start_time is not None:
+            deltat = np.clip(
+                t - self.start_time - self.window.seconds,
+                0.0,
+                np.inf
+            )
+            self.ax.set_xlim((deltat, deltat + self.window.seconds))
 
         return self.line
 
@@ -70,40 +75,54 @@ FPS = 30
 FRAME_TIME_SECONDS = 1 / FPS
 
 
-def run_data_collection_on_thread(sensors, start_time):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+async def create_cancellable_tasks(coroutines, task_set):
+    for coro in coroutines:
+        task_set.add(asyncio.create_task(coro))
 
     try:
-        loop.run_until_complete(
-            asyncio.gather(
-                *(
-                    sensor.start_collection(
-                        start_time=start_time,
-                        window_duration_seconds=10.0
-                    )
-                    for sensor in sensors
-                )
-            )
-        )
-    finally:
-        loop.close()
+        for task in task_set:
+            await task
+    except asyncio.CancelledError:
+        # questo è infatti il comportamento che vogliamo ottenere
+        pass
+
+
+def run_background_data_collection(coroutines, task_set):
+    asyncio.run(
+        create_cancellable_tasks(coroutines, task_set)
+    )
 
 
 async def main():
+    WINDOW_DURATION_SECONDS = 10.0
     fig, ax = plt.subplots(1)
-    sensor = SimulatedLineSensor(ax, window_size_seconds=3.0)
 
-    # qui dovrei far partire l'acquisizione del sensore
-    #   * se faccio un await sul sensor.start_collection()
-    #     blocco l'esecuzione su questo thread
-    #
-    #   * dovrei far partire l'esecuzione su un thread in background...
+    sensor = SimulatedLineSensor(ax, window_size_seconds=3.0)
+    sensors = [sensor]
+
+    collectors = [
+        sensor.collect(
+            start_time=time.perf_counter(),
+            duration_seconds=WINDOW_DURATION_SECONDS
+        )
+        for sensor in sensors
+    ]
+
+    task_group = set()
+
+    def cancel_tasks(_):
+        for task in task_group:
+            task.cancel()
+
     background_thread = threading.Thread(
-        target=run_data_collection_on_thread,
-        args=([sensor], time.perf_counter())
+        target=run_background_data_collection,
+        args=(
+            collectors,
+            task_group
+        )
     )
     background_thread.start()
+    fig.canvas.mpl_connect("close_event", cancel_tasks)
 
     def update(frame):
         return sensor.update_visualization(time.perf_counter())
@@ -111,8 +130,10 @@ async def main():
     ani = animation.FuncAnimation(
         fig=fig,
         func=update,
+        frames=int(WINDOW_DURATION_SECONDS/FRAME_TIME_SECONDS),
         interval=FRAME_TIME_SECONDS*1000,
-        cache_frame_data=False
+        cache_frame_data=False,
+        repeat=False
     )
     plt.show()
     background_thread.join()
