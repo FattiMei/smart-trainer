6ee6063f8ca4e9fe241b639825ee3ef403ecf5d0
FattiMei <teomei68@gmail.com>
2025-09-09 17:02:29 +0200

    Make window size a positional (non-optional) command line parameter

--- logger/main.py
+++ logger/main.py
@@ -11,33 +11,24 @@ import sensor
 from devscan import scan_for_devices
 
 
-DEFAULT_WINDOW_SIZE_SECONDS = 10.0
-
-
-def parse_window_parameters(default_window_size_seconds: float = DEFAULT_WINDOW_SIZE_SECONDS):
+def parse_window_parameters():
     parser = argparse.ArgumentParser(
         prog='logger',
         description='Collects and visualize data from UWB radar and Arduino boards'
     )
 
     arguments = [
-        ('-group'      , str  , 'Group name'                                     ),
-        ('-subject'    , str  , 'Subject name'                                   ),
-        ('-activity'   , str  , 'Activity name'                                  ),
-        ('-info'       , str  , 'Additional information'                         ),
-        ('-window_size', float, 'Collection window size in seconds'              ),
-        ('-delay'      , float, 'Delay in seconds before starting the collection'),
+        ('-group'     , str  , 'Group name'                                     ),
+        ('-subject'   , str  , 'Subject name'                                   ),
+        ('-activity'  , str  , 'Activity name'                                  ),
+        ('-info'      , str  , 'Additional information'                         ),
+        ('window_size', float, 'Collection window size in seconds'              ),
     ]
 
     for (name, type, help) in arguments:
         parser.add_argument(name, type=type, help=help)
 
-    args = parser.parse_args()
-
-    if args.window_size is None:
-        args.window_size = default_window_size_seconds
-
-    return args
+    return parser.parse_args()
 
 
 def get_collection_filename(args):
